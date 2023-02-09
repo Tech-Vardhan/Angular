c6745d77b76f84e3ab33d193a599433f34f9c5c9
Vardhan <harsh.gurjar@techextensor.com>
2023-02-09 12:34:48 +0530

    Error;

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -14,6 +14,9 @@ export class CourseCardComponent {
   
   @Input()
   course:Course;
+
+  @Input()
+  cardIndex : number
   
   @Output('courseSelected')
   courseSelected  = new EventEmitter<Course>();
